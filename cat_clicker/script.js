402890916e5225be81ad365ee031ae820e31a16e
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2016-02-25 17:46:22 -0300

    added css animations and styles

--- cat_clicker/script.js
+++ cat_clicker/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var self = this;
 
+  /* Primary Functions */
   function createElements() {
     this.$counter1 = $('<div>').attr('id','counter1').addClass('counter').html(0).hide();
     this.$counter2 = $('<div>').attr('id','counter2').addClass('counter').html(0).hide();
@@ -12,9 +13,13 @@ $(document).ready(function() {
   function setupBindings() {
     $('.cat').on("click", function() {
       increaseCounter($(this));
+      counterAnimation($(this))
     });
   }
+  createElements();
+  setupBindings();
 
+  /* Secondary Functions */
   function increaseCounter($el) {
     var $counter = $el.find('.counter');
     var clickCount = parseInt($counter.html()) + 1;
@@ -22,6 +27,11 @@ $(document).ready(function() {
     $el.closest('.cat-wrapper').find('.clicks').empty().append("Clicks: " + clickCount.toString());
   }
 
-  createElements();
-  setupBindings();
+  function counterAnimation($el) {
+    var $counter = $el.closest('.cat-wrapper').find('.clicks');
+    $counter.addClass('ativou');
+    setTimeout(function() {
+      $counter.removeClass('ativou');
+    }, 300 )
+  }
 });
