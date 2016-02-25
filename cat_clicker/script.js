147ae2d8f251eabcaa7af141ac5be9e9b02d2caa
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2016-02-25 16:54:31 -0300

    second implementation of cat clicker, with 2 elements and 2 counters

--- cat_clicker/script.js
+++ cat_clicker/script.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
+  var self = this;
 
   function createElements() {
-    this.$counter = $('<div>').addClass('counter').html(0).hide();
+    this.$counter1 = $('<div>').attr('id','counter1').addClass('counter').html(0).hide();
+    this.$counter2 = $('<div>').attr('id','counter2').addClass('counter').html(0).hide();
 
-    $('.cat').append($counter);
+    $('#cat1').append($counter1);
+    $('#cat2').append($counter2);
   }
 
   function setupBindings() {
     $('.cat').on("click", function() {
-      increaseCounter();
+      increaseCounter($(this));
     });
   }
 
-  function increaseCounter() {
-    var clickCount = parseInt(this.$counter.html()) + 1;
-
+  function increaseCounter($el) {
+    var $counter = $el.find('.counter');
+    var clickCount = parseInt($counter.html()) + 1;
     $counter.html(clickCount);
-    
-    $('.clicks').empty().append("Clicks: " + clickCount.toString());
+    $el.closest('.cat-wrapper').find('.clicks').empty().append("Clicks: " + clickCount.toString());
   }
 
   createElements();
